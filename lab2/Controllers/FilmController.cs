87ca11563b1dfcf7c5403c83d89f2ffc6f5bd46a
Mikolaj Osiak <osiakmikolaj@gmail.com>
2024-11-11 17:52:04 +0100

    Dodaj pliki projektów.

---
+++ lab2/Controllers/FilmController.cs
@@ -0,0 +1,100 @@
+﻿using lab2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab2.Controllers
+{
+    public class FilmController : Controller
+    {
+        // Film list
+        private static IList<Film> films = new List<Film>
+        {
+            new Film(){Id = 1, Name = "The Shawshank Redemption", Description = "A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.", Price=10},
+            new Film(){Id = 2, Name = "The Godfather", Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", Price=9},
+            new Film(){Id = 3, Name = "The Dark Knight", Description = "When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.", Price=14},
+        };
+
+        // GET: FilmController
+        public ActionResult Index()
+        {
+            return View(films);
+        }
+
+        // GET: FilmController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(films.FirstOrDefault(x => x.Id == id));
+        }
+
+        // GET: FilmController/Create
+        public ActionResult Create()
+        {
+            return View(new Film());
+        }
+
+        // POST: FilmController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Film film)
+        {
+            try
+            {
+                film.Id = films.Count + 1;
+                films.Add(film);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FilmController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(films.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: FilmController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Film film)
+        {
+            try
+            {
+                film = films.FirstOrDefault(x => x.Id == id);
+                film.Name = film.Name;
+                film.Description = film.Description;
+                film.Price = film.Price;
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FilmController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(films.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: FilmController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Film film)
+        {
+            try
+            {
+                film = films.FirstOrDefault(x => x.Id == id);
+                films.Remove(film);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ lab2/Models/Film.cs
@@ -0,0 +1,10 @@
+﻿namespace lab2.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
